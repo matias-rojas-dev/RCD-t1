9d3fa69161254fa4189f50d2f8413681dcada994
Matías Rojas Rojas <mrojasr@utem.cl>
2022-04-15 23:13:36 -0400

    Subida de servidor SOAP y cliente

---
+++ WSClientSoap/WSClientSoap/HomeClient.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace WSClientSoap
+{
+    public partial class HomeClient : System.Web.UI.Page
+    {
+
+        WSServerSoap.MetodosRCDClient ws = new WSServerSoap.MetodosRCDClient();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void checkCode_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void name_result_Click(object sender, EventArgs e)
+        {
+            String name = name_inputCompleteName.Text.ToString();
+                String response = ws.ValidacionNombre(name);
+                name_response.Text = response;
+            
+
+            
+        }
+
+        protected void rut_result_Click(object sender, EventArgs e)
+        {
+            String rut = rut_withoutCheckCode.Text.ToString();
+            String dv = rut_checkCode.Text.ToString();
+            if ((ws.ValidacionRut(rut, dv).ToString()).Equals("True"))
+            {
+                rut_response.Text = "El digito validador ingresado es correcto";
+            }
+            else
+            {
+                rut_response.Text = "Oops! El digito verificador no corresponde";
+            }
+        }
+    }
+}
