56302dec89dec852877fa37cc452d09227ba4ee2
Vania Palacios <vpalacios@utem.cl>
2022-04-19 15:54:23 -0400

    Actualización de cliente

--- ClienteSoap/ClienteSoap/WebForm.aspx.cs
+++ ClienteSoap/ClienteSoap/WebForm.aspx.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Collections;
 
 namespace ClienteSoap
 {
     public partial class WebForm : System.Web.UI.Page
     {
-        WebService.WSValidarClient ws = new WebService.WSValidarClient();
+        wsServicioCliente.WSValidarClient ws = new wsServicioCliente.WSValidarClient();
+        //WebService.WSValidarClient ws = new WebService.WSValidarClient();
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -31,9 +33,30 @@ namespace ClienteSoap
 
         protected void Button2_Click(object sender, EventArgs e)
         {
-            String nombre = TextBox3.Text.ToString();
-            String a = ws.ValidacionNombre(nombre);
-            Label1.Text = "Resultado: " + a;
+            String nombreCompleto = TextBox3.Text.ToString();
+            String estructura;
+            int ok = 0;
+            String nombres = "Nombres: ", apellidos="Apellidos: ";
+            for (int i=0; i<ws.ValidacionNombre(nombreCompleto).Length; i++)
+            {
+                if (ok == 0 && ws.ValidacionNombre(nombreCompleto)[i] != "/")
+                {
+                    nombres += ws.ValidacionNombre(nombreCompleto)[i];
+                    nombres += " ";
+                }
+                if (ws.ValidacionNombre(nombreCompleto)[i] == "/")
+                {
+                    ok = 1;
+                    i++;
+                }
+                if (ok == 1)
+                {
+                    apellidos += ws.ValidacionNombre(nombreCompleto)[i];
+                    apellidos += " ";
+                }
+            }
+            Label1.Text = nombres + " </br> " + apellidos;
+                
         }
     }
 }
