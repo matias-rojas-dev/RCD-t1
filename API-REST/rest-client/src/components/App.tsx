fc43e4b3e7ffc52aacb90f19aa9ef7b475761b50
David Uribe <daviduribe998@gmail.com>
2022-04-25 15:27:24 -0400

    Información del grupo

--- API-REST/rest-client/src/components/App.tsx
+++ API-REST/rest-client/src/components/App.tsx
@@ -12,14 +12,19 @@ function App(): JSX.Element {
   return (
     <Card>
       <h1 className="mb-2 text-lg font-semibold text-white">Web service - API REST</h1>
-      <p className="mb-6 text-sm text-slate-300">
+      <p className="text-sm text-slate-300">
         Ingrese su nombre completo para separarlo en nombres y apellidos paterno y materno. Además,
         ingrese su RUT (con dígito verificador) para validarlo.
       </p>
-      <div className="flex flex-col gap-6">
+      <div className="mt-6 flex flex-col gap-6">
         <SplitNameForm />
         <ValidateRutForm />
       </div>
+      <p className="mt-6 text-xs leading-normal text-blue-300/20">
+        Trabajo realizado por el grupo 3 de la asignatura de Redes y Comunicación de Datos,
+        integrado por Maximiliano Araya, Benjamín Herrera, Vania Palacios, Matías Rojas y David
+        Uribe.
+      </p>
     </Card>
   );
 }
