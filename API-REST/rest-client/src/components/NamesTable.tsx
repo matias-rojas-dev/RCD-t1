0abf4145cda78ddba08173df239548d026bc1abb
David Uribe <daviduribe998@gmail.com>
2022-04-25 19:35:52 -0400

    Actualización menor

--- API-REST/rest-client/src/components/NamesTable.tsx
+++ API-REST/rest-client/src/components/NamesTable.tsx
@@ -4,7 +4,7 @@ function HeaderCell({ className, ...props }: HeaderCellProps): JSX.Element {
   return (
     <div
       className={`flex h-full items-center
-      border-blue-400/5 bg-blue-300/[2%]
+      border-blue-400/5 bg-blue-300/[3%]
       px-3 py-2 font-semibold text-slate-100 ${className}`}
       {...props}
     />
@@ -34,26 +34,22 @@ function NamesTable({
 }: NamesTableProps): JSX.Element {
   return (
     <div
-      className="my-6 grid rounded-xl border-2 border-blue-400/5 bg-clip-border text-sm text-white"
-      style={{ gridTemplateColumns: "max-content auto" }}
+      className="my-6 rounded-xl border-2 border-blue-400/5 bg-clip-border text-sm text-white"
       {...props}
     >
-      <HeaderCell className="col-span-2 rounded-t-lg border-b-2">Nombres</HeaderCell>
-      <div className="col-span-2">
+      <HeaderCell className="rounded-t-lg border-b-2">Nombres</HeaderCell>
+      <div>
         {names.map(name => (
           <Cell key={name} className="border-b-2">
             {name}
           </Cell>
         ))}
       </div>
-      <HeaderCell className="col-span-2 border-b-2">Apellidos</HeaderCell>
-      <div
-        className="col-span-2 grid grid-cols-2"
-        style={{ gridTemplateColumns: "max-content auto" }}
-      >
+      <HeaderCell className="border-b-2">Apellidos</HeaderCell>
+      <div className="grid grid-cols-[max-content_auto]">
         <HeaderCell className="border-b-2 border-r-2">Apellido paterno</HeaderCell>
-        <Cell className="border-b-2 border-r-2">{paternal}</Cell>
-        <HeaderCell>Apellido materno</HeaderCell>
+        <Cell className="border-b-2">{paternal}</Cell>
+        <HeaderCell className="border-r-2">Apellido materno</HeaderCell>
         <Cell>{maternal}</Cell>
       </div>
     </div>
